8993ec58b89f6c72f2e57b095efe8b3e20393b37
Alexandre <alexandre.santana@dce.ufpb.br>
2018-09-02 11:43:14 -0300

    teste1 passsou

--- src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
@@ -7,6 +7,7 @@ public class Jogo {
 			throw new ExcecaoJogo("A bolsa de tiles deve ser fornecida na criação de uma partida");
 		}
 		
+		
 		if(sequencia.length < 2) {
 			throw new ExcecaoJogo("Cada partida deve ter uma sequência de pelo menos dois jogadores");
 		}
